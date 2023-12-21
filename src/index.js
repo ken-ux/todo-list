d3215804b6218b6d148a3de7df5ab208225dbb67
Kenny Nguyen <sokenns@gmail.com>
2023-12-20 17:46:01 -0800

    Import item and list classes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./style.css";
+import ToDoItem from "./ToDoItem";
+import ToDoList from "./ToDoList";
+
+let testItem1 = new ToDoItem("task_1", "task_desc", "date", "high");
+let testItem2 = new ToDoItem("task_2", "task_desc", "date", "high");
+let testItem3 = new ToDoItem("task_1", "task_desc", "date", "high");
+
+let list = new ToDoList();
+
+list.addToList(testItem1);
+list.addToList(testItem2);
+list.addToList(testItem3);
+
+list.removeFromList(testItem2);
+console.log(list.items);
