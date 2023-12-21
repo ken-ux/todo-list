677d1039a12eded168a00717860d75b45bed6f10
Kenny Nguyen <sokenns@gmail.com>
2023-12-20 18:11:10 -0800

    Format dates in setter

--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -1,9 +1,20 @@
+import { format } from "date-fns";
+
 export default class ToDoItem {
   constructor(title, description, dueDate, priority) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
+  }
+
+  get dueDate() {
+    return this._dueDate;
+  }
+
+  // Make date readable
+  set dueDate(date) {
+    this._dueDate = format(date, "yyyy-MM-dd");
   }
 
   completed = false;
