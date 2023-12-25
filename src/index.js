68dca5c4648a20ba96108a102ded6f6d1be39295
Kenny Nguyen <sokenns@gmail.com>
2023-12-24 16:43:08 -0800

    Move storage functions into separate module

--- src/index.js
+++ src/index.js
@@ -1,30 +1,10 @@
 import "./style.css";
-import ToDoItem from "./ToDoItem";
-import ToDoList from "./ToDoList";
 import createSidebar from "./sidebar";
 import displayList from "./ListItems";
 import { newItemModal, newListModal } from "./AddModals";
+import { intializeLists } from "./storageFuncs"
 
-let toDoLists = [];
-
-if (localStorage.getItem("storedLists")) {
-  toDoLists = JSON.parse(localStorage.getItem("storedLists"));
-  for (let i = 0; i < toDoLists.length; i++) {
-    Object.setPrototypeOf(toDoLists[i], ToDoList.prototype);
-  }
-} else {
-  let item = new ToDoItem(
-    "Your first to-do item!",
-    "This is a description of what you need to do.",
-    new Date(2025, 1, 11),
-    "high"
-  );
-
-  let list = new ToDoList("Default List");
-  list.addToList(item);
-  toDoLists.push(list);
-  localStorage.setItem("storedLists", JSON.stringify(toDoLists));
-}
+let toDoLists = intializeLists();
 
 const content = document.querySelector("#content");
 content.appendChild(createSidebar(toDoLists));
