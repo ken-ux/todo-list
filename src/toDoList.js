4ce181bdb1862be39f0bce6ff5cd220a6ed7813d
Kenny Nguyen <sokenns@gmail.com>
2023-12-25 01:21:39 -0800

    Create todo list class

---
+++ src/toDoList.js
@@ -0,0 +1,15 @@
+export default class ToDoList {
+  constructor(name) {
+    this.name = name;
+    this.items = new Array();
+  }
+
+  addToList(item) {
+    this.items.push(item);
+  }
+
+  removeFromList(item) {
+    let index = this.items.indexOf(item);
+    this.items.splice(index, 1);
+  }
+}
