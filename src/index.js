b1f15d2d34cd041baede4e9b417676b037b8b54d
Kenny Nguyen <sokenns@gmail.com>
2023-12-26 01:02:50 -0800

    Create buttons for adding projects or to-dos

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ projects.push(new Project("default"));
 projects.push(new Project("default_2"));
 projects.push(new Project("default_3"));
 
+projects[0].addToProject(new ToDo("todo", "desc", "date", "priority"));
+projects[0].addToProject(new ToDo("todo_2", "desc", "date", "priority"));
+projects[0].addToProject(new ToDo("todo_3", "desc", "date", "priority"));
+
 const sidebar = document.querySelector("#sidebar");
 for (let i = 0; i < projects.length; i++) {
   const project = document.createElement("div");
@@ -13,7 +17,22 @@ for (let i = 0; i < projects.length; i++) {
   sidebar.appendChild(project);
 }
 
+const addProjectButton = document.createElement("button");
+addProjectButton.textContent = "Add Project";
+sidebar.appendChild(addProjectButton);
+
 const main = document.querySelector("main");
-let projectName = document.createElement("h1");
+const projectName = document.createElement("h1");
 projectName.textContent = projects[0].name;
-main.appendChild(projectName);
+main.prepend(projectName);
+
+const projectItems = document.querySelector("#project-items");
+for (let i = 0; i < projects[0].items.length; i++) {
+  const item = document.createElement("div");
+  item.textContent = projects[0].items[i].name;
+  projectItems.appendChild(item);
+}
+
+const addToDoButton = document.createElement("button");
+addToDoButton.textContent = "Add To-Do";
+projectItems.appendChild(addToDoButton);
