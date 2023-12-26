3d5681e3c308f3b1a2d9320a355fd8d83181a42f
Kenny Nguyen <sokenns@gmail.com>
2023-12-26 02:26:41 -0800

    Move classes into separate modules

---
+++ src/project.js
@@ -0,0 +1,27 @@
+export default class Project {
+  constructor(name) {
+    this.name = name;
+    this.items = new Array();
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (value === "") {
+      throw new TypeError("Name cannot be empty.");
+    } else {
+      this._name = value;
+    }
+  }
+
+  addToProject(item) {
+    this.items.push(item);
+  }
+
+  removeFromProject(item) {
+    let index = this.items.indexOf(item);
+    this.items.splice(index, 1);
+  }
+}
---
+++ src/todo.js
@@ -0,0 +1,25 @@
+export default class ToDo {
+  constructor(name, description, dueDate, priority) {
+    this.name = name;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.complete = false;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (value === "") {
+      throw new TypeError("Name cannot be empty.");
+    } else {
+      this._name = value;
+    }
+  }
+
+  toggleComplete() {
+    this.complete = !this.complete;
+  }
+}
