ff506b093c8a87e01b19c5773b553d4c3298c8bb
Kenny Nguyen <sokenns@gmail.com>
2023-12-26 00:15:20 -0800

    Throw error if item or project name isn't specified

--- src/appLogic.js
+++ src/appLogic.js
@@ -1,7 +1,19 @@
-export class ToDoList {
+export class ToDoProject {
   constructor(name) {
     this.name = name;
     this.items = new Array();
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (value === "") {
+      throw new TypeError("Name cannot be empty.");
+    } else {
+      this._name = value;
+    }
   }
 
   addToList(item) {
@@ -15,10 +27,27 @@ export class ToDoList {
 }
 
 export class ToDoItem {
-    constructor(name, description, dueDate, priority) {
-      this.name = name;
-      this.description = description;
-      this.dueDate = dueDate;
-      this.priority = priority;
+  constructor(name, description, dueDate, priority) {
+    this.name = name;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.complete = false;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (value === "") {
+      throw new TypeError("Name cannot be empty.");
+    } else {
+      this._name = value;
     }
   }
+
+  toggleComplete() {
+    this.complete = !this.complete;
+  }
+}
