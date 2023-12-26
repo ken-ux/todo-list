505f7955cdcede03a3a6663b042b1521eb44f90f
Kenny Nguyen <sokenns@gmail.com>
2023-12-26 00:15:39 -0800

    Add sidebar and main content

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import "./style.css";
+import { ToDoItem, ToDoProject } from "./appLogic";
+
+const toDoProjects = [];
+toDoProjects.push(new ToDoProject("default"));
+toDoProjects.push(new ToDoProject("default_2"));
+toDoProjects.push(new ToDoProject("default_3"));
+
+const sidebar = document.querySelector("#sidebar");
+for (let i = 0; i < toDoProjects.length; i++) {
+  const project = document.createElement("div");
+  project.textContent = toDoProjects[i].name;
+  sidebar.appendChild(project);
+}
+
+const main = document.querySelector("main");
+let projectName = document.createElement("h1");
+projectName.textContent = toDoProjects[0].name;
+main.appendChild(projectName);
